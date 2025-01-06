45db9cdd7d26d7929ac43bbed03ec3fc4a384a76
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2025-01-06 20:43:22 +0700

    Feat: handle connection closed

---
+++ src/http-exception/microservice-connection-exception.filter.ts
@@ -0,0 +1,34 @@
+import {
+  Catch,
+  ExceptionFilter,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+@Catch(Error)
+export class MicroserviceConnectionExceptionFilter implements ExceptionFilter {
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    if (exception.message.includes('Connection closed')) {
+      response.status(HttpStatus.SERVICE_UNAVAILABLE).json({
+        statusCode: HttpStatus.SERVICE_UNAVAILABLE,
+        message:
+          'The service you are trying to reach is currently unavailable. Please try again later.',
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+    } else {
+      // Handle other errors or rethrow the exception
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'An internal server error occurred.',
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './http-exception/http-exception.filter';
+import { MicroserviceConnectionExceptionFilter } from './http-exception/microservice-connection-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new MicroserviceConnectionExceptionFilter());
+  // app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
