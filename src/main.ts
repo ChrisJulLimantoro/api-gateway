c1185356cedb6a8038f279bea0b5e408e79d7ed8
ella <brigittaella15@gmail.com>
2025-01-08 12:24:02 +0700

    finance

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: 'http://localhost:5173',  // Allow your frontend's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Authorization',  // Allowed headers
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
