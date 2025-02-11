f6ed5daa93c65acc43bdae0f6522f0ed9167857a
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2025-02-11 13:13:41 +0700

    config: add something

--- src/http-exception/microservice-connection-exception.filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MicroserviceConnectionExceptionFilter } from './microservice-connection-exception.filter';
-
-describe('MicroserviceConnectionExceptionFilter', () => {
-  it('should be defined', () => {
-    expect(new MicroserviceConnectionExceptionFilter()).toBeDefined();
-  });
-});
