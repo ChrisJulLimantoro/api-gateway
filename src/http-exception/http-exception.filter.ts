506de9fc7c4b2c34eb3136b0d732993c2106bd00
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2025-03-17 12:06:34 +0700

    feat: catch for Token expired exception

--- src/http-exception/http-exception.filter.ts
+++ src/http-exception/http-exception.filter.ts
@@ -3,7 +3,6 @@ import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 @Catch()
 export class HttpExceptionFilter<T> implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {
-    console.log('Exception thrown', exception);
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     response.status(exception).json({
